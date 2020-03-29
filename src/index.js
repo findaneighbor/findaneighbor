9882dd088c4a5fee54ed1f6c71ece56e9563dda7
tehpsalmist <bstewardcodes@gmail.com>
2020-03-28 23:55:12 -0400

    chore: start er up

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 
 export const App = props => {
-  return <main>Hello World!</main>
+  return <main className='h-screen flex-center text-2xl text-blue-600'>Find a neighbor. Be a neighbor!</main>
 }
 
 render(<App />, document.getElementById('app'))
