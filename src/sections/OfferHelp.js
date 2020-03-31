d95ad4e30a6e4d56fab24ac0a45342778aa237a1
tehpsalmist <bstewardcodes@gmail.com>
2020-03-31 02:28:35 -0400

    fix: scroll location on navigation

--- src/sections/OfferHelp.js
+++ src/sections/OfferHelp.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { OfferForm } from '../components'
 
 export const OfferHelp = ({ className = '', style = {} }) => {
+  useEffect(() => {
+    window.scroll({
+      top: 0
+    })
+  }, [])
+
   return <div className='all min-h-md w-full'>
     <OfferForm />
   </div>
