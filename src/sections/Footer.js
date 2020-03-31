af8cc16eebb37e6b858452d3c532e9fab1c0334b
tehpsalmist <bstewardcodes@gmail.com>
2020-03-31 02:15:14 -0400

    chore: wrap footer in paragraph tag

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const year = new Date().getFullYear()
 
 export const Footer = ({ className = '', style = {} }) => {
-  return <div style={style} className={`footer text-secondary-500 ${className}`}>
-    &copy; Find A Neighbor {year}
+  return <div style={style} className={`footer flex-col text-secondary-500 ${className}`}>
+    <p>&copy; Find A Neighbor {year}</p>
   </div>
 }
