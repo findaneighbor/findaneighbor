efd31c64db2448435cf085d9502fb8230a30dc32
tehpsalmist <bstewardcodes@gmail.com>
2020-03-31 21:18:19 -0400

    chore: picture update

--- src/sections/Heading.js
+++ src/sections/Heading.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import backgroundImage from '../assets/subheader_quakertown.jpg'
 
 export const Heading = ({ className = '', style = {} }) => {
   return <header className='heading flex-col py-12 px-4'>
