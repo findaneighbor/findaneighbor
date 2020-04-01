ae1c298ffb5fa5f027d629c63fc6ef91f88a557a
tehpsalmist <bstewardcodes@gmail.com>
2020-04-01 12:25:10 -0400

    feat: show/hide needs

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,6 +1,6 @@
 export const NEED_TYPES = `
   query NeedTypes {
-    need_type(order_by: { order: asc, id: desc }) {
+    need_type(order_by: {order: asc, id: desc}, where: {_or: [{hidden: {_is_null: true}}, {hidden: { _eq: false}}]}) {
       id
       order
       label
