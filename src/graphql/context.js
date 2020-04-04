93e418c4b4e204e69ad6cd72023127cc87375293
tehpsalmist <bstewardcodes@gmail.com>
2020-04-04 17:14:35 -0400

    chore: local env

--- src/graphql/context.js
+++ src/graphql/context.js
@@ -4,7 +4,9 @@ import memCache from 'graphql-hooks-memcache'
 import { NEED_TYPES } from './queries'
 
 export const client = new GraphQLClient({
-  url: 'https://find-a-neighbor.herokuapp.com/v1/graphql',
+  url: process.env.NODE_ENV === 'production'
+    ? 'https://find-a-neighbor.herokuapp.com/v1/graphql'
+    : 'http://localhost:8080/v1/graphql',
   cache: memCache()
 })
 
--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ import { render } from 'react-dom'
 import { App } from './sections'
 import { BrowserRouter } from 'react-router-dom'
 
+if (process.env.NODE_ENV === 'development') {
+  document.title = 'Local | Find A Neighbor'
+}
+
 render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('app'))
