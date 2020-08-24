e265744fe904d17f67b57f201863bf06b6852902
tehpsalmist <bstewardcodes@gmail.com>
2020-08-23 23:12:11 -0400

    feat: google translate

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -7,6 +7,7 @@ export const Footer = ({ className = '', style = {} }) => {
   return <div style={style} className={`footer ${className}`}>
     <p className='email'>
       Questions? <a className='text-primary-500 underline' href='mailto:findaneighbor@gmail.com'>Email us.</a>
+      <div id='google_translate_element'></div>
     </p>
     <p className='give text-center'>
       <a className='text-primary-500 underline' href='https://escregistrations.churchcenter.com/giving/to/find-a-neighbor'>Give</a> to the Cause.
