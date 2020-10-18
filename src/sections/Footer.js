dfc776ecdd9482aac3676c69858cc9c0e02d9353
tehpsalmist <bstewardcodes@gmail.com>
2020-10-18 02:00:26 -0400

    fix: google translate div

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -5,10 +5,10 @@ const year = new Date().getFullYear()
 
 export const Footer = ({ className = '', style = {} }) => {
   return <div style={style} className={`footer ${className}`}>
-    <p className='email'>
+    <div className='email'>
       Questions? <a className='text-primary-500 underline' href='mailto:findaneighbor@gmail.com'>Email us.</a>
-      <span id='google_translate_element'></span>
-    </p>
+      <div id='google_translate_element'></div>
+    </div>
     <p className='give text-center'>
       <a className='text-primary-500 underline' href='https://escregistrations.churchcenter.com/giving/to/find-a-neighbor'>Give</a> to the Cause.
       <br />
